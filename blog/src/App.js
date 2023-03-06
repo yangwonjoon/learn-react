a895e2a021d806dcc89d435519f0e3ae28865001
diddnjswns <diddnjswns20@gmail.com>
2023-03-06 20:09:22 +0900

    react 2

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,12 +4,13 @@ import { useState } from "react";
 
 function App() {
   let [head, b] = useState([
-    "여자 코트 추천",
-    "남자 코트 추천",
-    "유아 코트 추천",
+    "라자 코트 추천",
+    "다자 코트 추천",
+    "가아 코트 추천",
   ]);
 
-  let [num, count] = useState(0);
+  let [num, count] = useState([0, 0, 0]);
+  let [modal, setmodal] = useState(false);
 
   return (
     <div className='App'>
@@ -25,28 +26,57 @@ function App() {
       >
         글자 변경
       </button>
+      <button
+        onClick={() => {
+          let copy = [...head];
+          copy.sort();
+          b(copy);
+        }}
+      >
+        가나다순 정렬
+      </button>
+
       <div className='list'>
-        <h4>
-          {head[0]}
-          <span
-            onClick={() => {
-              count(num + 1);
-            }}
-          >
-            👍
-          </span>
-          {num}
+        <h4
+          onClick={() => {
+            setmodal(!modal);
+          }}
+        >
+          {head[2]}
         </h4>
         <p>2월 17일 발행</p>
       </div>
-      <div className='list'>
-        <h4>{head[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{head[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
+
+      {head.map(function (a, i) {
+        return (
+          <div className='list' key={i}>
+            <h4>
+              {head[i]}
+              <span
+                onClick={() => {
+                  count(num + 1);
+                }}
+              >
+                👍
+              </span>
+              {num}
+            </h4>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
+
+      {modal == true ? <Modal /> : null}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세 내용</p>
     </div>
   );
 }
